f28bf3b8e7f282c0302f72e4675e3a76bc730894
Ben Brown <ralim@ralimtek.com>
2025-06-22 13:58:39 +1000

    Minify build

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,21 @@
 FROM rust:1-alpine AS builder
 WORKDIR /usr/src/
 RUN rustup update
-RUN rustup target add x86_64-unknown-linux-musl
 RUN apk add --no-cache musl-dev
 RUN USER=root cargo new fronius_meter_emulation
 WORKDIR /usr/src/fronius_meter_emulation
 COPY Cargo.toml Cargo.lock ./
+# Seed the cache from dependencies from the Cargo.* files
 RUN cargo build --release
-
+# Now load in the source code (Layer that changes more frequently)
 COPY src ./src
-RUN cargo install --target x86_64-unknown-linux-musl --path .
+RUN cargo install --path .
 
 # Bundle Stage
-FROM curlimages/curl
+FROM alpine:latest
+WORKDIR /usr/local/bin
 COPY --from=builder /usr/local/cargo/bin/fronius_meter_emulation .
 USER 1000
 EXPOSE 5502
-CMD ["./fronius_meter_emulation"]
 HEALTHCHECK CMD netstat -an | grep 5502 > /dev/null; if [ 0 != $? ]; then exit 1; fi;
+CMD ["/usr/local/bin/fronius_meter_emulation"]
--- src/shelly_3em_client.rs
+++ src/shelly_3em_client.rs
@@ -26,46 +26,6 @@ impl Shelly3EMClient {
             None
         }
     }
-
-    // async fn worker(target_device: SocketAddr, output: Sender<Readings>) {
-    //     // Loop reading the shelly and dumping it to the fake meter
-    //     loop {
-    //         // The Shelly samples the inputs at 1Hz
-    //         // Starting at 1011 (Total Current) we read 3 floats for (Total Current,Active Power,Apparent Power)
-
-    //         if let Ok(total_readings) = ctx.read_input_registers(1011, 2 * 3).await.unwrap() {
-    //             // Convert the bytes of the totals into floats and send onwards
-    //             let total_current = merge_u16_f32(total_readings[0], total_readings[1]);
-    //             let total_active_power = merge_u16_f32(total_readings[2], total_readings[3]);
-    //             let total_reactive_power = merge_u16_f32(total_readings[4], total_readings[5]);
-    //             println!("Shelly Readings {total_current}A {total_active_power}VA {total_reactive_power}VAR");
-    //             output
-    //                 .send(Readings::TotalRealPower(total_active_power))
-    //                 .await
-    //                 .expect("Cant send readings to fake meter");
-    //             output
-    //                 .send(Readings::ReactivePower(total_reactive_power))
-    //                 .await
-    //                 .expect("Cant send readings to fake meter");
-    //             output
-    //                 .send(Readings::NetACCurrent(total_current))
-    //                 .await
-    //                 .expect("Cant send readings to fake meter");
-    //         } else {
-    //             panic!("Cant Read Shelly input regs for total power")
-    //         }
-    //         send_phase_a_readings(&mut ctx, &output)
-    //             .await
-    //             .expect("Cant send readings to fake meter");
-    //         send_phase_b_readings(&mut ctx, &output)
-    //             .await
-    //             .expect("Cant send readings to fake meter");
-    //         send_phase_c_readings(&mut ctx, &output)
-    //             .await
-    //             .expect("Cant send readings to fake meter");
-    //         tokio::time::sleep(Duration::from_secs(1)).await;
-    //     }
-    // }
 }
 fn merge_u16_f32(a: u16, b: u16) -> f32 {
     let x: u32 = a as u32 | (b as u32) << 16;
